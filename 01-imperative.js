5fd61055c4d579c359f94f12e95049b6f4a6fd2f
najmusskb <najmussakib173@gmail.com>
2023-03-11 23:52:28 +0600

    odd

--- 01-imperative.js
+++ 01-imperative.js
@@ -1,7 +1,18 @@
 // imparative
 // declarative
 
+// const numbers = [1, 2, 12, 36, 5, 458, 5];
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i] * 2);
+// }
+// same kag ta travers system diye kora jay
+
 const numbers = [1, 2, 12, 36, 5, 458, 5];
-for (let i = 0; i < numbers.length; i++) {
-  console.log(numbers[i] * 2);
-}
+// numbers.forEach(() => {
+//   console.log();
+// });
+numbers.forEach(function (value) {
+  if (value % 2 === 0) {
+    console.log(value);
+  }
+});
