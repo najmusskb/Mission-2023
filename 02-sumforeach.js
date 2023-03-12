a7771f6e0c5947fc9c1530c6cd3f559565721e0d
najmusskb <najmussakib173@gmail.com>
2023-03-12 23:44:23 +0600

    create a new student id

--- 02-sumforeach.js
+++ 02-sumforeach.js
@@ -1,5 +1,4 @@
 const numbers = [1, 2, 12, 36, 5, 458, 5];
-
 let sum = 0;
 numbers.forEach(function (value, index) {
   if (index <= 3) {
---
+++ 03-imperarivewayfindNumber.js
@@ -0,0 +1,4 @@
+// 03-imperarive way find Number 
+const arr = [1, 2, 3, 4, 5, null, false, 4, 5, "test", 6, 7];
+
+for()  
